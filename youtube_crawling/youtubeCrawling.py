9196faf036bbecf65a51a049b1d3080764454646
junjungjae <junjungjae@naver.com>
2021-05-30 20:29:19 +0900

    Update youtubeCrawling.py

--- youtube_crawling/youtubeCrawling.py
+++ youtube_crawling/youtubeCrawling.py
@@ -1,17 +1,13 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-import urllib.request
 from bs4 import BeautifulSoup
 import re
 
 
-# chromedriver.exe 파일을 받아서 저장하시고, 해당 파일의 경로를 아래에 입력하세요.
-browser = webdriver.Chrome(executable_path="D:\chrome_download\chromedriver.exe")
+# chromedrive가 있는 경로
+browser = webdriver.Chrome(executable_path="../chromedriver.exe")
 
 # 유튜브 브라우저로 이동
 url = "https://www.youtube.com/"
@@ -20,19 +16,17 @@ browser.maximize_window()
 time.sleep(5)
 
 # 검색창에 커서클릭
-# (질문) find_element_by_xpath가 해당하는 객체의 전체 html 경로가 맞나요? => 맞음. 만약 exception 에러가 뜬다면 더 하위 태그로 내려가서 해당 태그의 full xpath를 따야됨
 elem = browser.find_element_by_xpath("""//*[@id="search"]""")
 
-# 검색어 입력 후 검색
-# 여러분이 각자 크롤링 할 부분을 나눠 놨으니, 검색어를 이곳에 입력하시면 됩니다.
-elem.send_keys("악기 취미")
-
+# 검색어 입력 후 검색버튼 클릭
+elem.send_keys("검색할 단어")
 browser.find_element_by_xpath("""//*[@id="search-icon-legacy"]/yt-icon""").click()
 time.sleep(3)
+
 # 필터 클릭
 browser.find_element_by_xpath("""//*[@id="container"]/ytd-toggle-button-renderer/a""").click()
 time.sleep(3)
-# '올해' 필터 클릭 - 저희는 게시한 지 1년 미만의 동영상만 사용합니다.
+# '올해' 필터 클릭
 browser.find_element_by_xpath(
     """/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/
     div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/
@@ -41,14 +35,15 @@ time.sleep(3)
 
 # --------------------------------------------------------------------------------------
 # body pane을 자동 scroll해주는 코드
-body = browser.find_element_by_tag_name('body')  # (질문) 해당 페이지의 처음과 끝의 body 태그를 잡는게 맞나요? => 맞음
+body = browser.find_element_by_tag_name('body')
 
-height = browser.find_element_by_xpath("""/html/body/ytd-app/div/ytd-page-manager/ytd-search""").size['height']  # (질문) .size['height'] 이 부분이 잘 이해 안가요 => 댓글창 div의 전체 높이
+# 현재 페이지의 높이
+height = browser.find_element_by_xpath("""/html/body/ytd-app/div/ytd-page-manager/ytd-search""").size['height']
 while True:
-    for i in range(30):  # (질문) range 30까지 잡는 이유가 위에서부터 댓글 30개만 크롤링해오는게 맞나요? => 계속 아래로 스크롤 하다보면 위쪽 엘리멘트가 업로드가 안되서 일부러 다시 스크롤을 올리는 거
+    for i in range(30):  # url 크롤링을 위해 키다운 입력을 통해 페이지 스크롤하는 과정. 너무 많아지면 버벅거려 30으로 제한
         body.send_keys(Keys.PAGE_DOWN)
     sizecheck = browser.find_element_by_xpath("""/html/body/ytd-app/div/ytd-page-manager/ytd-search""").size['height']
-    if height == sizecheck:
+    if height == sizecheck: # 기존 height 입력값과 비교하여 같으면 break, 다르면 height 갱신 후 계속 진행
         break
     height = sizecheck
 
@@ -63,35 +58,28 @@ linklists = []
 
 for element in soup.find_all("div", class_="text-wrapper style-scope ytd-video-renderer"):
     for link in element.find_all("a"):
-        hreflink = link.get("href")
-        if hreflink[1:6] == 'watch':  # (질문) 따온 링크에서 해당 부분을 watch로 바꿔주는 이유가 있을까요?
+        hreflink = link.get("href") # url이 저장된 태그 검색 후 리스트에 추가
+        if hreflink[1:6] == 'watch':
             linklists.append(hreflink)
 
 print(linklists)
 
-# 주의사항
-# with open에서 "w"로 파일을 열었을 경우, 기존 파일 내용을 완전히 지우고 새로 쓴다는 의미입니다.
-# 따라서 크롤링 중간에 에러가 났고, 에러부분을 건너뛴 후,
-# 기존 tsv파일 밑으로 이어서 작업하고 싶으시다면
-# 반드시 아래 with open ~ 부분 4줄을 지우거나, 주석처리한 후 코드를 실행해 주십시오.
-
-# -----------------------------------------------------------------------------------------------------
-with open("유튜브_댓글테이블.txt", "w", encoding="utf-8-sig") as f:
+# ----------------------해당 형식을 가지는 메모장 저장하는 부분--------------------------------------------------
+with open("../유튜브_댓글테이블.txt", "w", encoding="utf-8-sig") as f:
     f.write('동영상_제목\t댓글_내용\t댓글_작성시간\n')
 
-with open("유튜브_동영상테이블.txt", "w", encoding="utf-8-sig") as f:
+with open("../유튜브_동영상테이블.txt", "w", encoding="utf-8-sig") as f:
     f.write('동영상_제목\t동영상_게시일\t댓글_수\t조회수\t좋아요\t싫어요\t동영상_설명_본문\t동영상_URL\n')
 # -----------------------------------------------------------------------------------------------------
 
-browser = webdriver.Chrome(executable_path="C:\Caba2015\chromedriver.exe")
+browser = webdriver.Chrome(executable_path="../chromedriver.exe")
 browser.maximize_window()
 
 linklists_count = -1
 for hreflink in linklists:
-
     linklists_count += 1
 
-    # 해당 print는 어느 url에서 error가 발생했는지 체크하는 포인트입니다.
+    # 해당 print는 어느 url에서 error가 발생했는지 체크하는 포인트
     print("#####" + str(linklists_count) + "번째 linklists 인덱스 작업 중 #####\n")
 
     url = "https://www.youtube.com" + hreflink
@@ -104,7 +92,7 @@ for hreflink in linklists:
     browser.find_element_by_xpath("""/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div""").size[
         'height']
     while True:
-        for i in range(1000):
+        for i in range(1000): # 1000번의 키다운을 통해 댓글 로딩
             body.send_keys(Keys.PAGE_DOWN)
 
         time.sleep(1)
@@ -117,8 +105,8 @@ for hreflink in linklists:
 
     videoNameXpath = """/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/h1/yt-formatted-string"""
     videoName = browser.find_element_by_xpath(videoNameXpath).text
-    videoName = re.sub(r"[^ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z0-9 ]", " ", videoName)
-    videoName = re.sub(" {2,}", " ", videoName)  # (질문) 위에 부분은 정규표현식 써서 제거하는건 알겠는이 이 줄은 잘 이해가 안가요 => 2칸 이상의 공백이 존재할 경우 1개의 공백으로 변환
+    videoName = re.sub(r"[^ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z0-9 ]", " ", videoName) # 정규표현식 통해 제목의 특수문자나 기타 언어 제거
+    videoName = re.sub(" {2,}", " ", videoName)
     print("동영상 제목: " + videoName)
 
     videoDateXpath = """/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/div[1]/div[2]/yt-formatted-string"""
@@ -130,8 +118,7 @@ for hreflink in linklists:
     try:
         commentNum = browser.find_element_by_xpath(commentNumXpath).text
 
-    except NoSuchElementException as e:
-        # (질문) 이 except 과정이 댓글이 없을 경우 다시 스크롤을 올려서 기록하는? 그런 과정이 맞나요? => 위에서 말했다시피 엘리멘트가 로딩이 안되서 다시 위로 1000번 스크롤링하는 과정
+    except NoSuchElementException as e: # 댓글 로딩 중 너무 오래 걸려 해당 element를 못찾은 경우로 다시 스크롤을 위로 올려 로딩 해소
         for i in range(1000):
             body.send_keys(Keys.PAGE_UP)
         time.sleep(1)
@@ -158,7 +145,7 @@ for hreflink in linklists:
     videoText = re.sub(" {2,}", " ", videoText)
     print("본문 내용: " + videoText)
 
-    with open("유튜브_동영상테이블.txt", "a", encoding="utf-8-sig") as f:
+    with open("../유튜브_동영상테이블.txt", "a", encoding="utf-8-sig") as f:
         f.write(
             '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(videoName, videoDate, commentNum, viewCount, goodCount, badCount,
                                                       videoText, url))
@@ -178,7 +165,7 @@ for hreflink in linklists:
             print(date)
 
             if comment != " ":
-                with open("유튜브_댓글테이블.txt", "a", encoding="utf-8-sig") as f:
+                with open("../유튜브_댓글테이블.txt", "a", encoding="utf-8-sig") as f:
                     f.write('{}\t{}\t{}\n'.format(videoName, comment, date))
 
         except NoSuchElementException as e:
